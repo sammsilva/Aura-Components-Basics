6c930868960b8787433d31b2b55f454e845e0646
Magalhaes Silva <slml@gft.com>
2020-01-29 16:02:58 -0200

    controller - inserting item already working - still need to validate fields

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -1,13 +1,15 @@
 ({
     clickCreateItem : function(cmp, event, helper) {
+        var iName = cmp.get("v.newItem.Name");       
+        var iQuantity = cmp.get("v.newItem.Quantity__c");
+        var iPrice = cmp.get("v.newItem.Price__c");
 
-        var iName = cmp.find('iname').get("v.value");
-        console.log(iName.length);
-        if(iName.length==0)
-            return;
-        
-        var iPrice = cmp.find('iprice').get("v.value");
-        console.log(iPrice);
 
+        var newItemRef = cmp.get("v.newItem");
+        console.log("Create Camping Item: " + JSON.stringify(newItemRef));        
+        var myItems = cmp.get("v.items");
+        myItems.push(newItemRef);
+        console.log("Items in List: " + JSON.stringify(cmp.get("v.items"))); 
+        cmp.set("v.items", myItems);       
     }
 })
